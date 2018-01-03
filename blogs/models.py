78069bb945ed8d008c299508d6529657a9cedfee
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2018-01-03 21:30:33 +0100

    Removed the posts field from the Blog model. Instead, a blog field has been added in the Post model.

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,4 @@
+# from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
@@ -9,24 +10,8 @@ class Category(models.Model):
         return self.name
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=130)
-    summary = models.TextField()
-    content = models.TextField()
-    image = models.FileField(blank=True, null=True)
-    video = models.FileField(blank=True, null=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.title
-
-
 class Blog(models.Model):
-    blog_name = models.CharField(max_length=40)    # URLField()
+    blog_name = models.CharField(max_length=60)
     author = models.CharField(max_length=40)
     photo = models.URLField(blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
@@ -34,10 +19,29 @@ class Blog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
-    posts = models.ForeignKey(Post, on_delete=models.PROTECT)
+    # posts = ArrayField(models.ForeignKey(Post, on_delete=models.PROTECT))
+    #  ArrayField cannot be used to relate classes
+    # posts = models.ManyToManyField(Post, related_name='posts')
 
     def __str__(self):
         return self.blog_name
+
+
+class Post(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.PROTECT)
+    title = models.CharField(max_length=130)
+    summary = models.TextField()
+    content = models.TextField()
+    image = models.URLField(blank=True, null=True)
+    video = models.URLField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
 
 
 class User(models.Model):
