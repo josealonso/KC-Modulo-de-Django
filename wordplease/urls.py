aa6644096a1619a3b2487f7c618418bd6cb97931
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2018-01-04 23:50:51 +0100

    Added a basic login to the application

--- wordplease/urls.py
+++ wordplease/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from users.views import login
 from blogs.views import home, blogs, post_detail
 
 from django.contrib import admin
@@ -22,6 +22,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login', login, name="login_page"),
     path('blogs/', blogs, name='blogs_page'),
     path('posts/<int:pk>', post_detail, name="post_detail_page"),
     path('', home, name='home_page'),
