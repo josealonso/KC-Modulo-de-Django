79b67cdbfd5532e3e12d0358631fbc624913226f
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2018-01-05 12:49:23 +0100

    Replaced the traditional login form by a Django form.

---
+++ users/forms.py
@@ -0,0 +1,7 @@
+from django.forms import forms
+
+
+class LoginForm(forms.Form):
+
+    login_username = forms.CharField(label="Nombre de usuario")
+    login_password = forms.CharField(label="Contraseña")
