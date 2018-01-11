95504150b0d5c6dfbd330b776dbd2ba92eeda04a
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2018-01-11 01:04:21 +0100

    Implemented endpoint for creating a user

--- users/serializers.py
+++ users/serializers.py
@@ -1,17 +1,33 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class UserSerializer(serializers.Serializer):
 
+    # Actúa como un traductor de objetos a tipos primitivos (lo contrario a lo que hace un formulario)
     id = serializers.ReadOnlyField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     username = serializers.CharField()
-    email = serializers.CharField()
+    email = serializers.EmailField()
     password = serializers.CharField()
 
-    def create(self, validated_data):
-        pass
+    def validate_username(self, data):
+        if User.objects.filter(username=data).exists():
+            raise ValidationError("El usuario ya existe")
+        return data
+
+    def create(self, validated_data):  # Construye un objeto User
+        instance = User()
+        instance.first_name = validated_data.get("first_name")
+        instance.last_name = validated_data.get("last_name")
+        instance.username = validated_data.get("username")
+        instance.email = validated_data.get("email")
+        instance.set_password(validated_data.get("password"))
+        # El método "set_password" cifra la contraseña
+        instance.save()
+        return instance
 
     def update(self, instance, validated_data):
         pass
