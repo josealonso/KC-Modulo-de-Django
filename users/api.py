e3d9370233e7855c00a360e87043116121ba431a
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2018-01-12 01:43:19 +0100

    Implemented endpoint for updating the details of a user

--- users/api.py
+++ users/api.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UsersListSerializer
 
 
 class HelloWorld(APIView):
@@ -22,14 +22,14 @@ class UsersListAPI(APIView):
 
     def get(self, request):
         users = User.objects.all()   # users es un objeto que hay que convertir al formato de salida
-        serializer = UserSerializer(users, many=True)
+        serializer = UsersListSerializer(users, many=True)
         # El serializador obtiene un diccionario por cada usuario de la lista
         return Response(serializer.data)
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
+            user = serializer.save()  # Llama a create o a update
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -41,4 +41,15 @@ class UserDetailAPI(APIView):
         # user = User.objects.all().filter(pk=pk)
         user = get_object_or_404(User, pk=pk)
         serializer = UserSerializer(user)
+        # Llama al método create de serializers.py
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        # Llama al método update de serializers.py
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- users/serializers.py
+++ users/serializers.py
@@ -3,9 +3,14 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 
-class UserSerializer(serializers.Serializer):
-
+class UsersListSerializer(serializers.Serializer):
     # Actúa como un traductor de objetos a tipos primitivos (lo contrario a lo que hace un formulario)
+    id = serializers.ReadOnlyField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+
+
+class UserSerializer(UsersListSerializer):
     id = serializers.ReadOnlyField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
@@ -14,12 +19,19 @@ class UserSerializer(serializers.Serializer):
     password = serializers.CharField()
 
     def validate_username(self, data):
-        if User.objects.filter(username=data).exists():
+        # if User.objects.filter(username=data).exists():
+        if self.instance is None and User.objects.filter(username=data).exists():
+            # La primera condición se cumple cuando se está creando un usuario
             raise ValidationError("El usuario ya existe")
+        if self.instance and self.instance.username == data and User.objects.filter(username=data).exists():
+            raise ValidationError("Este nombre de usuario ya existe")
         return data
 
     def create(self, validated_data):  # Construye un objeto User
         instance = User()
+        return self.update(instance, validated_data)
+
+    def update(self, instance, validated_data):
         instance.first_name = validated_data.get("first_name")
         instance.last_name = validated_data.get("last_name")
         instance.username = validated_data.get("username")
@@ -29,5 +41,4 @@ class UserSerializer(serializers.Serializer):
         instance.save()
         return instance
 
-    def update(self, instance, validated_data):
-        pass
+
